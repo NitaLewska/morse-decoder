77e7f04f828670324ba29912f3b04b060f71521b
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-10-04 01:19:23 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -37,8 +37,25 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const MORSE_TABLE_encoded = {
+};
+
+for (const [key, value] of Object.entries(MORSE_TABLE)) {
+    let new_key = []
+    key.split('').map((a) => {a === '.' ? new_key.push(10) : new_key.push(11)});
+    new_key = new_key.join('').padStart(10, '0');
+    MORSE_TABLE_encoded[new_key] = value;
+  }
+
+  MORSE_TABLE_encoded['**********'] = ' '
+
 function decode(expr) {
-    // write your solution here
+    expr = expr.match(/.{1,10}/g) || []
+    let output = []
+    expr.map((a) => {
+        output.push(MORSE_TABLE_encoded[a])
+    })
+    return output.join('')
 }
 
 module.exports = {
